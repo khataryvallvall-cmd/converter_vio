feed505f01800225d5175e8955ad493ab2abe63b
khataryvallvall-cmd <khataryvallvall@gmail.com>
2026-08-12 00:45:18 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,14 @@ class ConverterVioApp extends StatelessWidget {
       title: 'Converter Vio',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.dark,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: const TextScaler.linear(1.0),
+          ),
+          child: child!,
+        );
+      },
       home: const MainScreen(),
     );
   }
